d20d88065f79a8f4a1cb7299f75c0a9874ac1806
Futa HIRAKOBA <korosuke613613@gmail.com>
2022-09-14 13:15:23 +0900

    chore: setting required versions

--- terraform.tf
+++ terraform.tf
@@ -3,6 +3,14 @@ terraform {
     bucket = "terraform-state-korosuke613"
     prefix = "terraform-gcp-korosuke613"
   }
+
+  required_version = "~> 1.2.8"
+
+  required_providers {
+    google = {
+      version = "~> 4.34"
+    }
+  }
 }
 
 provider "google" {
